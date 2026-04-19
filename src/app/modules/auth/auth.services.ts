efdc7fa26e990805a851696ed7f702800bef6dbd
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-04-19 11:25:34 +0530

    change: keyid to hash of public key & use keyid header in accessTokens

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,15 +1,20 @@
 import { exportJWK, importSPKI } from "jose";
+import { createHash } from "node:crypto";
 import { readFileSync } from "node:fs";
 
 export const getJwks = async () => {
-  const publicKeyPem = readFileSync(process.env.PUBLIC_KEY_PATH!, "utf-8");
+  const publicKey = readFileSync(process.env.PUBLIC_KEY_PATH!, "utf-8");
 
-  const ecPublicKey = await importSPKI(publicKeyPem, "RS256");
+  const ecPublicKey = await importSPKI(publicKey, "RS256");
   const jwk = await exportJWK(ecPublicKey);
+  const kid = createHash("sha256")
+    .update(publicKey)
+    .digest("hex")
+    .substring(0, 16);
   const jwks = [
     {
       ...jwk,
-      kid: "iris_main_key",
+      kid: kid,
       use: "sig",
       alg: "RS256",
       kty: "RSA",
--- src/app/modules/auth/utils/token.ts
+++ src/app/modules/auth/utils/token.ts
@@ -1,13 +1,20 @@
 import JWT from "jsonwebtoken";
 import { readFileSync } from "node:fs";
 import type { UserTokenPayload } from "../../../common/utils/interfaces";
+import { createHash } from "node:crypto";
 
 const privateKey = readFileSync(process.env.PRIVATE_KEY_PATH!, "utf-8");
+const publicKey = readFileSync(process.env.PUBLIC_KEY_PATH!, "utf-8");
 
 export function createAccessToken(payload: UserTokenPayload) {
+  const kid = createHash("sha256")
+    .update(publicKey)
+    .digest("hex")
+    .substring(0, 16);
   return JWT.sign(payload, privateKey, {
     algorithm: "RS256",
     expiresIn: "15m",
     issuer: "iris-auth",
+    keyid: kid,
   });
 }
