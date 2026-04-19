09bb0164cb30815c40a28dee931e9bfb64e2bd91
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-04-19 10:37:28 +0530

    add: openid config endpoint

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,17 +1,26 @@
 import express from "express";
+import ApiResponse from "./common/utils/api-response";
 
-export async function createApplication() {
+export function createApplication() {
   const app = express();
 
   app.use(express.json());
 
   app.get("/", (_, res) => {
-    return res.json({ message: "Welcome to iris." });
+    ApiResponse.ok(res, "Welcome to iris.");
   });
 
   app.get("/.well-known/openid-configuration", (_, res) => {
-    
-  })
+    return res.status(200).json({
+      issuer: process.env.AUTH_BASE_URL || "http://localhost:8080",
+      authorization_endpoint:
+        `${process.env.AUTH_BASE_URL}/auth/login` ||
+        "http://localhost:8080/auth/login",
+      jwks_uri:
+        `${process.env.AUTH_BASE_URL}/certs` ||
+        "http://localhost:8080/auth/certs",
+    });
+  });
 
   return app;
 }
